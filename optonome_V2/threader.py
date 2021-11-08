dba00dda2f9627d8e36c64216ea62a4b25082b20
Daniel Luoma (TAU) <daniel.luoma@tuni.fi>
2021-11-08 16:53:18 +0200

    deleted threader class as it is obsolete for time being

--- optonome_V2/threader.py
+++
@@ -1,7 +0,0 @@
-import threading
-
-class thread(threading.Thread):
-    def __init__(self, thread_name, function):
-        threading.Thread.__init__(self)
-        self.thread_name = thread_name
-        self.function = function
